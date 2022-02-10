7aa5b461845059bef50b02e2b754a1ebc7499db0
hichemBA67 <hichem.benaoun@hotmail.com>
2022-02-10 15:22:37 +0100

    9. Palaindrome_number.js (Solution 2)

--- Solutions/Easy/9_Palaindrome_number.js
+++ Solutions/Easy/9_Palaindrome_number.js
@@ -2,6 +2,8 @@
  * @param {number} x
  * @return {boolean}
  */
+
+// Solution 1
 var isPalindrome = function (x) {
   if (x > 0x7fffffff) {
     return fasle;
@@ -12,4 +14,26 @@ var isPalindrome = function (x) {
   if (result === x) return true;
 
   return false;
+};
+
+// Solution 2
+var isPalindrome = function (x) {
+  if (x === 0) {
+    return true;
+  } else if (x < 0 || x % 10 === 0) {
+    return false;
+  } else {
+    reversed = 0;
+    while (x > reversed) {
+      let pop = x % 10;
+      x = Math.trunc(x / 10);
+      reversed = reversed * 10 + pop;
+    }
+
+    if (x === reversed || x === Math.trunc(reversed / 10)) {
+      return true;
+    } else {
+      return false;
+    }
+  }
 };
